14852d6b6e23abe983e2efce9d0f2e1e4c5d21ac
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-11-12 23:01:47 +0100

    release 1.1.0.3

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 # use this PROD script to prepare the release of the bundle
 #
 export VERSION="1.1.0"
+export BUILD="3"
 set -e
 set -o pipefail
 
 npm run build
 mvn clean install
 git add .
-git commit -m"release $VERSION.2"
+git commit -m"release $VERSION.$BUILD"
 git push heroku master
 git push origin master
