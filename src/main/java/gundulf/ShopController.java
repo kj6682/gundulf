0cc6f051a8c82c884eab3c0a2b95193ec0e89e0e
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-12-31 10:34:45 +0100

    add milan

--- src/main/java/gundulf/ShopController.java
+++ src/main/java/gundulf/ShopController.java
@@ -3,7 +3,9 @@ package gundulf;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 class ShopController {
@@ -11,16 +13,22 @@ class ShopController {
     @Value("${PROXY_ORDERS}")
     private String proxyOrders;
 
-    @RequestMapping("/paris")
-    public String paris(Model model) {
+    @RequestMapping("/shop/{shop}")
+    public String paris(@PathVariable String shop, Model model) {
+
+
+        if(SHOP.LUXEMBOURG.toString().equals(shop.toUpperCase())){
+
+            return getModel(SHOP.LUXEMBOURG, model);
+        }
+
+        if(SHOP.MILAN.toString().equals(shop.toUpperCase())){
+
+            return getModel(SHOP.MILAN, model);
+        }
+
         return getModel(SHOP.PARIS, model);
     }
-
-    @RequestMapping("/luxembourg")
-    public String luxembourg(Model model) {
-        return getModel(SHOP.LUXEMBOURG, model);
-    }
-
 
     private String getModel(SHOP shop, Model model) {
         model.addAttribute("shop", shop.name);
@@ -30,7 +38,7 @@ class ShopController {
 
 
     private enum SHOP {
-        PARIS("paris"), LUXEMBOURG("luxembourg");
+        PARIS("paris"), LUXEMBOURG("luxembourg"), MILAN("milan");
 
         private String name;
 
