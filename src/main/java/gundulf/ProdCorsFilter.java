42183517ee0e24cba76d0f513b6750c8af635d06
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2018-01-20 23:07:36 +0100

    release 1.1.0.29

--- src/main/java/gundulf/ProdCorsFilter.java
+++ src/main/java/gundulf/ProdCorsFilter.java
@@ -3,7 +3,6 @@ package gundulf;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
 
-@Profile({"dev"})
+@Profile({"heroku"})
 @Component
-public class SimpleCorsFilter implements Filter {
+public class ProdCorsFilter implements Filter {
 
-    private final Logger log = LoggerFactory.getLogger(SimpleCorsFilter.class);
+    private final Logger log = LoggerFactory.getLogger(ProdCorsFilter.class);
 
 
     @Override
@@ -28,7 +27,7 @@ public class SimpleCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         //response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "https://gundulf-chef.herokuapp.com");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
