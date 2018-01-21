b27c307109fa403aa5d81e67f67e3f84118fdb28
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2018-01-21 01:51:25 +0100

    release 1.1.0.33

--- src/main/java/gundulf/ProdCorsFilter.java
+++ src/main/java/gundulf/ProdCorsFilter.java
@@ -10,7 +10,10 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 
 @Profile({"heroku"})
 @Component
@@ -26,15 +29,23 @@ public class ProdCorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        //response.setHeader("Access-Control-Allow-Origin", "*");
-        //response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Origin", "https://gundulf-chef.herokuapp.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        String[] allowDomain = {"http://gundulf-chef.herokuapp.com", "https://gundulf-chef.herokuapp.com"};
+        Set<String> allowedOrigins = new HashSet<>(Arrays.asList(allowDomain));
+        String originHeader = request.getHeader("Origin");
 
+        if (allowedOrigins.contains(originHeader)) {
+            //response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+            //response.setHeader("Access-Control-Allow-Origin", "https://gundulf-chef.herokuapp.com");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers",
+                               "Content-Type, Accept, X-Requested-With, remember-me");
+        }
         chain.doFilter(req, res);
+
+
     }
 
     private void debug(HttpServletRequest request) {
